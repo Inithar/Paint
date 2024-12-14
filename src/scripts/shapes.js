c6e63ec2ad7807e682e4cf67d93ab80a6738595f
Szymon Świtała <szymon.switala@proton.me>
2024-12-14 17:59:15 +0100

    feat: add the ability to draw square, rectangle and hexagon

--- src/scripts/shapes.js
+++ src/scripts/shapes.js
@@ -11,7 +11,10 @@ import {
 const shapes = ["line", "square", "circle", "triangle", "rectangle", "hexagon"];
 
 const draw = {
-  line: () => drawLine()
+  line: () => drawLine(),
+  square: () => drawPolygon(4, Math.PI / 4),
+  rectangle: () => drawPolygon(4, Math.PI / 4),
+  hexagon: () => drawPolygon(6, Math.PI)
 };
 
 export function handleShapeButtonClick(shapeNumber) {
@@ -43,4 +46,44 @@ function drawLine() {
   context.beginPath();
   context.moveTo(mouseDownPosition.x, mouseDownPosition.y);
   context.lineTo(currentMousePosition.x, currentMousePosition.y);
+}
+
+function drawPolygon(sides, angle) {
+  const context = getContext();
+  const radius = getRadius();
+  const currentShape = getCurrentShape();
+  const mouseDownPosition = getMouseDownPosition();
+  let currentAngle = angle;
+
+  const coordinates = [];
+
+  const multiplyBy = currentShape === "rectangle" ? 2 : 1;
+
+  for (let i = 0; i < sides; i++) {
+    const x = mouseDownPosition.x + multiplyBy * radius * Math.cos(currentAngle);
+    const y = mouseDownPosition.y - radius * Math.sin(currentAngle);
+
+    coordinates.push({ x, y });
+
+    currentAngle += (2 * Math.PI) / sides;
+  }
+
+  context.beginPath();
+  context.moveTo(coordinates[0].x, coordinates[0].y);
+
+  for (let i = 1; i < sides; i++) {
+    context.lineTo(coordinates[i].x, coordinates[i].y);
+  }
+
+  context.closePath();
+}
+
+function getRadius() {
+  const mouseDownPosition = getMouseDownPosition();
+  const currentMousePosition = getCurrentMousePosition();
+
+  return Math.sqrt(
+    Math.pow(mouseDownPosition.x - currentMousePosition.x, 2) +
+      Math.pow(mouseDownPosition.y - currentMousePosition.y, 2)
+  );
 }
